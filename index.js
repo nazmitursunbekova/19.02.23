960959e6af055e4a458d45e86b58c724420e394b
Tursunbekova Nazmi <nazmitursunbekova@gmail.com>
2023-02-21 13:59:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function nameIs(name) {
+  console.log("my name is " + name);
+}
+function surnameIs(name) {
+ console.log("my surname is " + name);
+}
+
+nameIs("Nazmi");
+surnameIs("Tursunbekova");
